0a7252ddd60e189c08dba847c47d7b30d3a37c08
usernomason <24204@daehyun.hs.kr>
2026-04-09 08:55:02 +0900

    Add text and image routes to Flask app

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
 from flask import Flask, jsonify
-import sys
 import random
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=["GET", "POST"])
-def index():
-    response = {
+@app.route('/text', methods=["GET", "POST"])
+def text_skill():
+    return jsonify({
         "version": "2.0",
         "template": {
             "outputs": [{
                 "simpleText": {
-                    "text": 
-
-str(random.randint(1,10))
+                    "text": str(random.randint(1, 10))
                 }
             }]
         }
-    }
-    return jsonify(response)
+    })
+
+@app.route('/image', methods=["GET", "POST"])
+def image_skill():
+    return jsonify({
+        "version": "2.0",
+        "template": {
+            "outputs": [{
+                "simpleImage": {
+                    "imageUrl": "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+                    "altText": "hello I'm Ryan"
+                }
+            }]
+        }
+    })
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
